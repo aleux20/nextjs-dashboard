caf9f570e98ef8aab0a2ba50eda83c52cb1939f9
Hans Afanador <hansalejandroca@gmail.com>
2025-03-25 16:12:32 -0500

    Chapter 16

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,12 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import LoginForm from "@/app/ui/login-form";
+import { Metadata } from "next";
 import { Suspense } from "react";
+
+export const metadata:Metadata = {
+    title: "Login - Acme",
+    description: "Sign in to your Acme account.",
+};
 
 export default function LoginPage() {
     return (
